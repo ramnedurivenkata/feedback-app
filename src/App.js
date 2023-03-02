d89dfa59788ae815193df2c473821f32ad621e4a
Pothu Raju <pothu_neduri@persistent.com>
2023-03-02 13:47:44 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData"
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
+import Card from "./components/shared/Card";
+function App(){
+    const [feedback, setFeedback] = useState(FeedbackData)
+
+    const deleteFeedback=(id)=>{
+        if(window.confirm('Are you sure you want to delete?'))
+        setFeedback(feedback.filter((item)=>item.id!==id))
+    }
+    return (
+       <> 
+            <Header/>
+            <div className="container">
+                <FeedbackForm/>
+                <FeedbackStats feedback={feedback}/>
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+            </div>
+        </>
+    )
+}
+export default App;
+
+
+
+
+
+
+
+
+
+//     const title='Blog Post'
+//     const body='This is my blog post'
+//     const comments=[
+//         {id:1,text:'Comment one'},
+//         {id:2,text:'Comment two'},
+//         {id:3,text:'Comment three'},
+//     ]
+//     const loading=false
+//     const showComments=true
+//     const commentBlock=(
+//         <div className="comments">
+//         <h3>Comments({comments.length})</h3>
+//         <ul>
+//             {comments.map((comment,index)=>(
+//                     <li key={index}>{comment.text}</li>
+//             ))}
+//         </ul>
+//     </div>
+//    )
+//     if(loading) return <h1>Loading...</h1>
